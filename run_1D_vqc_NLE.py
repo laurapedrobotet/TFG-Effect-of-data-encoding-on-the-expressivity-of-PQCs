c9ae96c8c85f172a08b0e6e619176a2837e91d6a
Laura Pedro-Botet <105119463+laurapedrobotet@users.noreply.github.com>
2025-06-23 14:33:01 +0200

    Add fitting for Burgers' eq using a PQC with different encoding strategies

--- run_1D_vqc_NLE.py
+++ run_1D_vqc_NLE.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# SOLUTION FOR BURGERS' EQUATION USING A VQC WITH 1 QUBIT AND NLE
+# FITTING FOR BURGERS' EQUATION USING A PQC WITH 1 QUBIT
 
 import os
 
@@ -26,7 +26,7 @@ import matplotlib.cm as cm
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="2D Burgers equation with Universal QML Circuit with batching"
+        description="1D Burgers' equation fitting"
     )
     parser.add_argument(
         "--time", type=float, default=0,
@@ -53,33 +53,7 @@ def parse_args():
 
 # -------------------------------------- Extra functions ---------------------------
 
-# Non-linear encoding
-
-def scaled_tanh_encoding(x, a, b):
-    """
-    f(x) = (π/2) * (tanh(a * x + b) + 1)
-    """
-    return (np.pi / 2) * (np.tanh(a * x + b) + 1)
-
-def scaled_softsign_encoding(x, a, b):
-    """
-    f(x) = π * (a * x + b) / (1 + |a * x + b|)
-    """
-    z = a * x + b
-    return np.pi * z / (1 + np.abs(z))
-
-def scaled_sigmoid_encoding(x, a):
-    """
-    f(x) = π / (1 + exp(-a * x))
-    """
-    return np.pi / (1 + np.exp(-a * x))
-
-def cap_encoding(x, a, b):
-    """
-    f(x) = clip(a * x + b, 0, π)
-    """
-    return np.clip(a * x + b, 0, np.pi)
-
+# Encoding strategies
 def loglin_encoding(x, a, b):
     """
     f(x) = clip(a * x + b * sign(x) * log(|x| + 1e-6), 0, π)
@@ -87,32 +61,16 @@ def loglin_encoding(x, a, b):
     eps = 1e-6
     log_part = b * np.sign(x) * np.log(np.abs(x) + eps)
     return np.clip(a * x + log_part, 0, np.pi)
-
-def logistic_encoding(x, a, b, k):
+    
+def scaled_tanh_encoding(x, a, b):
     """
-    f(x) = a * x + b * (1 / (1 + exp(k * x))) * (1 - x)
+    f(x) = (π/2) * (tanh(a * x + b) + 1)
     """
-    logistic = 1 / (1 + np.exp(k * x))
-    return a * x + b * logistic * (1 - x)
+    return (np.pi / 2) * (np.tanh(a * x + b) + 1)
 
 
 
 def one_layer(x, params):
-    """
-    Quantum layer that encodes two classical inputs (x and t) into quantum rotations 
-    using parameterized nonlinear and linear functions.
-
-    Inputs:
-    -----------
-    x (float or array-like): Spatial variable.
-    t (float or array-like): Time variable
-    params (list or array-like of 8 floats)
-        Trainable parameters used in the encoding:
-        - params[0], params[1]: parameters for non-linear RY(x) encoding 
-        - params[2], params[3]: parameters for non-linear RZ(x) encoding
-        - params[4], params[5]: parameters for linear RY(t) encoding
-        - params[6], params[7]: parameters for linear RZ(t) encoding
-    """
     qml.RY(scaled_tanh_encoding(x, params[0], params[1]), wires=0)
     qml.RZ(scaled_tanh_encoding(x, params[2], params[3]), wires=0)
 
@@ -137,13 +95,11 @@ def def_loss(x_train, u_train, nlayers):
     def rnd_param_init(seed=None):
         if seed is not None:
             np.random.seed(seed)
-        params = qml.numpy.array(np.random.uniform(low=0, high=2 * np.pi, size=(nlayers, 8)), requires_grad=True)
+        params = qml.numpy.array(np.random.uniform(low=0, high=2 * np.pi, size=(nlayers, 4)), requires_grad=True)
 
         
         params[:,0] = (params[:,0] - np.pi) / 100
         params[:,2] = (params[:,2] - np.pi) / 100
-        # params[:,4] = (params[:,4] - np.pi) / 100
-        # params[:,6] = (params[:,6] - np.pi) / 100
         return params
 
     def cost(x, u, params):
@@ -163,7 +119,6 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     args = parse_args()
-    # Create a dictionary of all parsed arguments
     args_dict = vars(args)
 
     time = str(int(args.time)) if args.time.is_integer() else str(args.time)
@@ -246,13 +201,13 @@ def main():
     with open(os.path.join(save_path, "configuration.json"), "w") as json_file:
         json.dump(args_dict, json_file, indent=4)
 
-    # Save final parameters in human-readable JSON format
+    # Save final parameters in JSON format
     param_dict = {f"Layer_{i+1}": layer_params.tolist() for i, layer_params in enumerate(params)}
     with open(os.path.join(save_path, "final_params.json"), "w") as f:
         json.dump(param_dict, f, indent=4)
 
 
-    # Plot results
+    # Plotting
     u_predict = []
     for x in x_train:
         u_predict.append(circuit(x,params))
@@ -263,7 +218,7 @@ def main():
     plt.legend()  
     plt.savefig("VQC_1D_Prediction.pdf")
 
-    # Plots
+
     plt.figure(figsize=(7, 7 * 6 / 8), dpi=300)
     plt.plot(
         x_train, 
@@ -288,7 +243,6 @@ def main():
     plt.legend()
     plt.savefig(os.path.join(save_path, "Predictions.pdf"), bbox_inches="tight")
 
-    # Plot Loss
     plt.figure()
     plt.semilogy(train_losses, label="Train loss")
     plt.semilogy(test_losses, label="Test loss", linestyle="--")
