e61b214a542dd34e987a2b22dbc4cad470821dd9
Laura Pedro-Botet <105119463+laurapedrobotet@users.noreply.github.com>
2025-06-23 14:46:11 +0200

    Add bash script to submit parallel jobs for NLE configurations

--- run_2D_NLE.sh
+++ run_2D_NLE.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 #SBATCH --chdir=/scratch/nas/4/laurap/pinn_qml/scripts
-#SBATCH --output=logs/nonlinearEnc80_%A_%a.out
+#SBATCH --output=logs/nonlinearEnc50_%A_%a.out
 #SBATCH --job-name=nonlinear_vqc
 #SBATCH --array=0-59
 #SBATCH --mem=16G
 
-# Layer and seed lists
 layers=(1 2 4 6 8 10)
 seeds=($(seq 0 9))
 
-# Compute indices
 layer_index=$((SLURM_ARRAY_TASK_ID / 10))
 seed_index=$((SLURM_ARRAY_TASK_ID % 10))
 
@@ -19,7 +17,7 @@ random_seed=${seeds[$seed_index]}
 
 echo "Running job $SLURM_ARRAY_TASK_ID with nlayers=$nlayers and seed=$random_seed on host $(hostname)"
 
-python3 run_80.py \
+python3 run_2D_vqc_general_enc.py \
     --encoding nonlinear \
     --nlayers "$nlayers" \
     --npoints 1000 \
